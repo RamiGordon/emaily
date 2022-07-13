dd7c9fddfa7a3920350c2f5dd262cf0076308640
Ramiro Gordon <ramirogordon2@gmail.com>
2022-07-12 23:03:19 -0400

    cookie-session working

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
-
-mongoose.connect(keys.mongoURI);
 
 // order of these lines is important
 require("./models/User");
 require("./services/passport");
 
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+  cookieSession({
+    // 30 days in milliseconds
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5001;
-
 app.listen(PORT);
