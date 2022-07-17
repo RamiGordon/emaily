17ab0d79e21dafb1acd18fcf0e69a92c4f4c2983
Ramiro Gordon <ramirogordon2@gmail.com>
2022-07-17 15:10:58 -0400

    survey campaing working :)

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,8 +2,10 @@ import _ from "lodash";
 import React from "react";
 import { connect } from "react-redux";
 import formFields from "./formFields";
+import { withRouter } from "react-router-dom";
+import * as actions from "../../actions";
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
   const reviewFields = _.map(formFields, ({ label, name }) => {
     return (
       <div key={name}>
@@ -23,7 +25,10 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
       >
         Back
       </button>
-      <button className="green btn-flat right white-text">
+      <button
+        className="green btn-flat right white-text"
+        onClick={() => submitSurvey(formValues, history)}
+      >
         Send Survey
         <i className="material-icons right">email</i>
       </button>
@@ -35,4 +40,4 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
