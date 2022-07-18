0c5f5d560d9d6fd98855e032cfbec9e632341d59
Ramiro Gordon <ramirogordon2@gmail.com>
2022-07-18 00:32:00 -0400

    surveyForm using ES2017

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,13 +1,13 @@
 import _ from "lodash";
-import React, { Component } from "react";
+import React from "react";
 import { reduxForm, Field } from "redux-form";
 import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
 import validateEmails from "../../utils/validateEmails";
 import formFields from "./formFields";
 
-class SurveyForm extends Component {
-  renderFields() {
+const SurveyForm = ({ handleSubmit, onSurveySubmit }) => {
+  const renderFields = () => {
     return _.map(formFields, ({ label, name }) => {
       return (
         <Field
@@ -19,27 +19,25 @@ class SurveyForm extends Component {
         />
       );
     });
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
-          {this.renderFields()}
-          <Link to="/surveys" className="red btn-flat white-text">
-            Cancel
-          </Link>
-          <button type="submit" className="teal btn-flat right white-text">
-            Next
-            <i className="material-icons right">done</i>
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSurveySubmit)}>
+        {renderFields()}
+        <Link to="/surveys" className="red btn-flat white-text">
+          Cancel
+        </Link>
+        <button type="submit" className="teal btn-flat right white-text">
+          Next
+          <i className="material-icons right">done</i>
+        </button>
+      </form>
+    </div>
+  );
+};
 
-function validate(values) {
+const validate = (values) => {
   const errors = {};
 
   errors.recipients = validateEmails(values.recipients || "");
@@ -51,7 +49,7 @@ function validate(values) {
   });
 
   return errors;
-}
+};
 
 export default reduxForm({
   validate,
