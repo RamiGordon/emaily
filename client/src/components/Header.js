ceb1c3773845a9a57c79ae3888f167bad5122b8e
Ramiro Gordon <ramirogordon2@gmail.com>
2022-07-18 00:58:10 -0400

    Header and Payments updated with ES2017

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,9 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Payments from "./Payments";
 
-class Header extends Component {
-  renderContent() {
-    switch (this.props.auth) {
+const Header = ({ auth }) => {
+  const renderContent = () => {
+    switch (auth) {
       case null:
         return;
       case false:
@@ -20,36 +20,27 @@ class Header extends Component {
             <Payments />
           </li>,
           <li key="3" style={{ margin: "0 10px" }}>
-            Credits: {this.props.auth.credits}
+            Credits: {auth.credits}
           </li>,
           <li key="2">
             <a href="/api/logout">Logout</a>
           </li>,
         ];
     }
-  }
-
-  render() {
-    return (
-      <nav>
-        <div className="nav-wrapper">
-          <Link
-            to={this.props.auth ? "/surveys" : "/"}
-            className="left brand-logo"
-          >
-            Emaily
-          </Link>
-          <ul className="right">{this.renderContent()}</ul>
-        </div>
-      </nav>
-    );
-  }
-}
-
-function mapStateToProps({ auth }) {
-  return {
-    auth,
   };
-}
+
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <Link to={auth ? "/surveys" : "/"} className="left brand-logo">
+          Emaily
+        </Link>
+        <ul className="right">{renderContent()}</ul>
+      </div>
+    </nav>
+  );
+};
+
+const mapStateToProps = ({ auth }) => ({ auth });
 
 export default connect(mapStateToProps)(Header);
